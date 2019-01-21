ebdd08f74f3790159fffc6a360b820e5ad28dddf
Adrien Pyke <adpyke@gmail.com>
2019-01-21 11:48:08 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+	'extends': 'adpyke-react',
+	'rules': {
+		'react-native/no-unused-styles': 2,
+		'react-native/split-platform-components': 2,
+		'react-native/no-inline-styles': 2
+	},
+	'plugins': [
+		'react',
+		'react-native'
+	]
+};
