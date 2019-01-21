5531010559ee6eee75d7fdc9b769224f93fb4f3b
Adrien Pyke <adpyke@gmail.com>
2019-01-21 11:58:55 -0500

    add env

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ module.exports = {
 		'react-native/split-platform-components': 2,
 		'react-native/no-inline-styles': 2
 	},
+	'env': {
+		'browser': false,
+		'node': true,
+		'react-native/react-native': true
+	},
 	'plugins': [
 		'react',
 		'react-native'
