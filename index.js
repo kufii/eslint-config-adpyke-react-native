82ba3a06ffce3fc4639a52b806addc6d9ab2b992
Adrien Pyke <adpyke@gmail.com>
2019-01-22 16:39:07 -0500

    turn off forbid-component-props

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 module.exports = {
 	'extends': 'adpyke-react',
 	'rules': {
+		'react/forbid-component-props': 0,
 		'react-native/no-unused-styles': 2,
 		'react-native/split-platform-components': 2,
 		'react-native/no-inline-styles': 2
