f9703cd7da40d86a7213c88ed28f202dbde7d787
Adrien Pyke <adpyke@gmail.com>
2019-09-07 19:08:30 -0400

    prettier

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 'use strict';
 
 module.exports = {
-	'extends': 'adpyke-react',
-	'rules': {
+	extends: 'adpyke-react',
+	rules: {
 		'react/forbid-component-props': 0,
 		'react-native/no-unused-styles': 2,
 		'react-native/split-platform-components': 2,
 		'react-native/no-inline-styles': 2
 	},
-	'env': {
-		'browser': false,
-		'node': true,
+	env: {
+		browser: false,
+		node: true,
 		'react-native/react-native': true
 	},
-	'plugins': [
-		'react',
-		'react-native'
-	]
+	plugins: ['react', 'react-native']
 };
